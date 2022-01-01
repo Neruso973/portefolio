ddc54ef54bb4637636a6baed31027af37b76ea47
Neruso973 <nelson.ntepp@icloud.com>
2022-01-01 16:13:54 +0100

    add tailwind to project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+
+function Navbar() {
+    return (
+        <div>
+            <img src="" alt="logo de Nelson" />
+            <li>
+                <ul>A Propos</ul>
+                <ul>Compétences</ul>
+                <ul>Formations</ul>
+                <ul>Projets</ul>
+                <ul>CV</ul>
+            </li>
+        </div>
+    )
+}
+
+export default Navbar
