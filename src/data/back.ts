d2e8d40040deac81058615631d9651a5f6b1ede8
Neruso973 <nelson.ntepp@icloud.com>
2022-01-05 08:41:03 +0100

    wip for skills

---
+++ src/data/back.ts
@@ -0,0 +1,21 @@
+import express from '../assets/express.png';
+import mysql from '../assets/mysql.png';
+import nest from '../assets/nest.png';
+
+export const front = [
+    {
+        logo: express,
+        className: 'h-24 m-2',
+        alt: "html logo"
+    },
+    {
+        logo: mysql,
+        className: 'h-24 w-16 m-2',
+        alt: "mysql logo"
+    },
+    {
+        logo: nest,
+        className: 'h-24 m-2',
+        alt: "nest logo"
+    },
+]
---
+++ src/data/tools.ts
@@ -0,0 +1,27 @@
+import git from '../assets/git.png';
+import xd from '../assets/Xd.png';
+import photoshop from '../assets/Photoshop.png';
+import office from '../assets/office.png';
+
+export const front = [
+    {
+        logo: git,
+        className: 'h-24 m-2',
+        alt: "git logo"
+    },
+    {
+        logo: xd,
+        className: 'h-24 w-16 m-2',
+        alt: "Adobe Xd logo"
+    },
+    {
+        logo: photoshop,
+        className: 'h-24 m-2',
+        alt: "adobe Photoshop logo"
+    },
+    {
+        logo: office,
+        className: 'h-24 m-2',
+        alt: "office logo"
+    },
+]
