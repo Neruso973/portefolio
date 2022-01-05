36d20cd386deaf20aeedad27c72f0b459bd61fa9
Neruso973 <nelson.ntepp@icloud.com>
2022-01-05 19:42:57 +0100

    skills back-end

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,39 +1,61 @@
-import { useState } from 'react';
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
-import {front} from '../data/front';
+import { front } from "../data/front";
+import { back } from "../data/back";
 
 function Skills() {
-    const [handleFrontBack, setHandleFrontBack] = useState<Boolean>(false)
-    const handleFB = () => {
-        setHandleFrontBack(!handleFrontBack)
-    }
+  const [isFlipped, setIsFlipped] = useState<boolean>(false);
 
-    
-    return (
-        <div className="h-screen grid grid-rows-6 grid-flow-col gap-7 p-8">
-           <div className='bg-white row-start-1 row-span-4 col-start-1 col-span-1 m-8'>
-               <div className='flex flex-col'>
-                   <h3 className='text-center text-3xl'>Front-end Techno</h3>
-                   <div className='grid grid-cols-3 gap-3 p-4'>
-                       {front.map((item)=> <img src={item.logo} alt={item.alt} className={item.className} />)}
-                   </div>
-                    <button className="bottom-2 right-2" onClick={handleFB}>{handleFrontBack ? "Back-end" : "Front-end"}</button>
-               </div>
-               <div>
+  const handleIsFlipped = () => {
+    setIsFlipped(!isFlipped);
+  };
 
-               </div>
-           </div>
-           <div className='bg-black row-start-1 row-span-4 col-start-3 col-span-4 m-8'>
-
-           </div>
-           <div className='bg-white row-start-5 row-span-2 col-start-1 col-span-3 m-8'>
-
-           </div>
-           <div className='bg-black row-start-5 row-span-2 col-start-4 col-span-3 m-8'>
-
-           </div>
+  return (
+    <div className="h-screen grid grid-rows-6 grid-flow-col gap-7 p-8">
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div className="bg-white row-start-1 row-span-4 col-start-1 col-span-1 m-8">
+            <div className="flex flex-col">
+              <h3 className="text-center text-3xl">Front-end Techno</h3>
+              <div className="grid grid-cols-3 gap-3 p-4">
+                {front.map((item) => (
+                  <img
+                    src={item.logo}
+                    alt={item.alt}
+                    className={item.className}
+                  />
+                ))}
+              </div>
+              <button className="bottom-2 right-2" onClick={handleIsFlipped}>
+                Back-end
+              </button>
+            </div>
+          </div>
+        <div>
+          <div className="bg-white row-start-1 row-span-4 col-start-1 col-span-1 m-8">
+            <div className="flex flex-col">
+              <h3 className="text-center text-3xl">Back-end Techno</h3>
+              <div className="grid grid-cols-3 gap-3 p-4">
+                {back.map((item) => (
+                  <img
+                    src={item.logo}
+                    alt={item.alt}
+                    className={item.className}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <button className="bottom-2 right-2" onClick={handleIsFlipped}>
+            Front-end
+          </button>
         </div>
-    )
+      </ReactCardFlip>
+      <div className="bg-black row-start-1 row-span-4 col-start-3 col-span-4 m-8"></div>
+      <div className="bg-white row-start-5 row-span-2 col-start-1 col-span-3 m-8"></div>
+      <div className="bg-black row-start-5 row-span-2 col-start-4 col-span-3 m-8"></div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
