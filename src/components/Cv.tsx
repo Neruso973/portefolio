e2f9ea0b2e5e1cce4b2926e9df8a71d241d6d53e
Neruso973 <nelson.ntepp@icloud.com>
2022-01-12 14:54:12 +0100

    home_and_about

--- src/components/Cv.tsx
+++ src/components/Cv.tsx
@@ -1,8 +1,12 @@
+import cv from "../assets/CV\ nelson\ ntepp.pdf";
 
 function Cv() {
     return (
         <div className="h-screen" id="cv">
-            <h2>Cv works!</h2>
+            {/* <embed src={cv} className="h-[40rem] w-96"/> */}
+            <a href={cv} download="CV\ nelson\ ntepp.pdf">
+                <button> Telecharger le cv</button>
+            </a>
         </div>
     )
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -12,9 +12,10 @@ function Skills() {
   };
 
   return (
-    <div className="h-screen grid grid-rows-6 grid-flow-col gap-7 p-8">
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-          <div className="bg-white row-start-1 row-span-4 col-start-1 col-span-1 m-8">
+    <div className="flex flex-col h-screen place-content-around">
+      <div className="flex flex-row place-content-around">
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div className="bg-white ">
             <div className="flex flex-col">
               <h3 className="text-center text-3xl">Front-end Techno</h3>
               <div className="grid grid-cols-3 gap-3 p-4">
@@ -31,29 +32,34 @@ function Skills() {
               </button>
             </div>
           </div>
-        <div>
-          <div className="bg-white row-start-1 row-span-4 col-start-1 col-span-1 m-8">
-            <div className="flex flex-col">
-              <h3 className="text-center text-3xl">Back-end Techno</h3>
-              <div className="grid grid-cols-3 gap-3 p-4">
-                {back.map((item) => (
-                  <img
-                    src={item.logo}
-                    alt={item.alt}
-                    className={item.className}
-                  />
-                ))}
+          <div>
+            <div className="bg-white w-7/12">
+              <div className="flex flex-col">
+                <h3 className="text-center text-3xl">Back-end Techno</h3>
+                <div className="grid grid-cols-3 gap-3 p-4">
+                  {back.map((item) => (
+                    <img
+                      src={item.logo}
+                      alt={item.alt}
+                      className={item.className}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
+            <button className="bottom-2 right-2" onClick={handleIsFlipped}>
+              Front-end
+            </button>
           </div>
-          <button className="bottom-2 right-2" onClick={handleIsFlipped}>
-            Front-end
-          </button>
+        </ReactCardFlip>
+        <div>
+          <div className="bg-black h-96 w-96"></div>
         </div>
-      </ReactCardFlip>
-      <div className="bg-black row-start-1 row-span-4 col-start-3 col-span-4 m-8"></div>
-      <div className="bg-white row-start-5 row-span-2 col-start-1 col-span-3 m-8"></div>
-      <div className="bg-black row-start-5 row-span-2 col-start-4 col-span-3 m-8"></div>
+      </div>
+      <div className="flex flex-row">             
+      <div className="bg-white h-32 w-6/12 m-8 "></div>
+      <div className="bg-black h-2/12 w-2/12"></div>
+      </div> 
     </div>
   );
 }
